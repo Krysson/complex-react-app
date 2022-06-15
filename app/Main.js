19fb0fbf819938a42db514a872570d75d3eab1bc
Krysson <mgulla@krysson.com>
2022-06-15 12:24:14 -0400

    Inital Commit 6152022 1224

---
+++ app/Main.js
@@ -0,0 +1,22 @@
+import React from "react"
+import ReactDOM from "react-dom/client"
+
+// my components
+import Header from "./components/Header"
+import HomeGuest from "./components/HomeGuest"
+import Footer from "./components/Footer"
+
+function Main() {
+  return (
+    <>
+      <Header />
+
+      <HomeGuest />
+
+      <Footer />
+    </>
+  )
+}
+
+const root = ReactDOM.createRoot(document.querySelector("#app"))
+root.render(<Main />)
