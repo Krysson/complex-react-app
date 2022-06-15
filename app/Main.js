7019f1291e8f32b76fb83a2f47d88e02b248d123
Krysson <mgulla@krysson.com>
2022-06-15 13:03:06 -0400

    prettier format chagnes 6152022 1303

--- app/Main.js
+++ app/Main.js
@@ -1,6 +1,6 @@
 import React from "react"
 import ReactDOM from "react-dom/client"
-import {BrowersRouter,Routes,Route, BrowserRouter} from "react-router-dom"
+import { BrowersRouter, Routes, Route, BrowserRouter } from "react-router-dom"
 
 // my components
 import Header from "./components/Header"
@@ -8,7 +8,6 @@ import HomeGuest from "./components/HomeGuest"
 import Footer from "./components/Footer"
 import About from "./components/About"
 import Terms from "./components/Terms"
-
 
 function Main() {
   return (
