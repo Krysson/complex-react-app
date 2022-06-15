5315005c2e8a0264fc99d98c9bb13b3e7b1e3edf
Krysson <mgulla@krysson.com>
2022-06-15 12:55:55 -0400

    Post module 20 Commit 6152022 1255

--- app/Main.js
+++ app/Main.js
@@ -1,20 +1,26 @@
 import React from "react"
 import ReactDOM from "react-dom/client"
+import {BrowersRouter,Routes,Route, BrowserRouter} from "react-router-dom"
 
 // my components
 import Header from "./components/Header"
 import HomeGuest from "./components/HomeGuest"
 import Footer from "./components/Footer"
+import About from "./components/About"
+import Terms from "./components/Terms"
+
 
 function Main() {
   return (
-    <>
+    <BrowserRouter>
       <Header />
-
-      <HomeGuest />
-
+      <Routes>
+        <Route path="/" element={<HomeGuest />} />
+        <Route path="/about-us" element={<About />} />
+        <Route path="/terms" element={<Terms />} />
+      </Routes>
       <Footer />
-    </>
+    </BrowserRouter>
   )
 }
 
